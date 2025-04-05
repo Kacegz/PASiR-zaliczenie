4ab481ad40f632178393b0cf9d4f8fe19eefe589
Kacegz <90635502+Kacegz@users.noreply.github.com>
2025-04-05 20:55:27 +0200

    Add JWT token

--- src/main/java/com/example/zaliczenie/controller/UserController.java
+++ src/main/java/com/example/zaliczenie/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.zaliczenie.controller;
 
 import com.example.zaliczenie.model.User;
 import com.example.zaliczenie.repository.UserRepository;
+import com.example.zaliczenie.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -16,8 +17,17 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/register")
     public String registerUser(@RequestBody User user) {
+        if(user.getUsername().isEmpty() || user.getPassword().isEmpty()){
+            return "Username and password cannot be empty";
+        }
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return "Username already exists";
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "User registered successfully";
@@ -27,9 +37,15 @@ public class UserController {
     public String loginUser(@RequestBody User user) {
         User existingUser = userRepository.findByUsername(user.getUsername());
         if (existingUser != null && passwordEncoder.matches(user.getPassword(), existingUser.getPassword())) {
-            return "Login successful";
+            String token = jwtUtil.generateToken(user.getUsername());
+            return "Login successful. Token: " + token;
         } else {
             return "Invalid username or password";
         }
+    }
+    @PostMapping("/validate")
+    public Boolean validateToken(@RequestHeader("Authorization") String token) {
+        String username = jwtUtil.extractUsername(token);
+        return username != null && jwtUtil.validateToken(token, username);
     }
 }
---
+++ src/main/java/com/example/zaliczenie/util/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.example.zaliczenie.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${SECRET}")
+    private String secretKey;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, String username) {
+        return (extractUsername(token).equals(username) && !isTokenExpired(token));
+    }
+}
