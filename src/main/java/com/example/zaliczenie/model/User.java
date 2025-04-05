73998b00e0e550c1f841a8891bfe8a525338165f
Kacegz <90635502+Kacegz@users.noreply.github.com>
2025-04-05 20:55:14 +0200

    fix id

--- src/main/java/com/example/zaliczenie/model/User.java
+++ src/main/java/com/example/zaliczenie/model/User.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 @Document(collection = "users")
 public class User {
     @Id
+    @Indexed(unique = true)
     private String id;
     @Getter
     @Setter
