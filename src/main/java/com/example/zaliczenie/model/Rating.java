a86f8edc4cf701fead14c672c94e150b0579d2b1
Kacegz <90635502+Kacegz@users.noreply.github.com>
2025-04-30 15:47:22 +0200

    Message exception fix

--- src/main/java/com/example/zaliczenie/model/Rating.java
+++ src/main/java/com/example/zaliczenie/model/Rating.java
@@ -2,6 +2,7 @@ package com.example.zaliczenie.model;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,7 +18,7 @@ public class Rating {
     private String id;
     private String teaId;
     private String userId;
-    @NotNull
+    @NotBlank(message = "Must be between 1 and 5")
     @Min(1)
     @Max(5)
     private int score;
