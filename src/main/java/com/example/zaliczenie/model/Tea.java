f4f1385cc4202e5958f1495b1d5fef1de3be8f56
Kacegz <90635502+Kacegz@users.noreply.github.com>
2025-05-11 19:45:55 +0200

    remove unused imports

--- src/main/java/com/example/zaliczenie/model/Tea.java
+++ src/main/java/com/example/zaliczenie/model/Tea.java
@@ -1,6 +1,5 @@
 package com.example.zaliczenie.model;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
